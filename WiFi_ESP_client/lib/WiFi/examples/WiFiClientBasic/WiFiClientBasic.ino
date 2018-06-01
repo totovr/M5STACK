6bbe02551ca0c82203cc6dd18ad0460f769e1d5c
Antonio Vega <a.vegaramirez76@gmail.com>
2018-06-01 20:36:27 +0900

    Update

--- WiFi_ESP_client/lib/WiFi/examples/WiFiClientBasic/WiFiClientBasic.ino
+++ WiFi_ESP_client/lib/WiFi/examples/WiFiClientBasic/WiFiClientBasic.ino
@@ -1,23 +1,28 @@
-#include <Arduino.h>
-#include "WiFi.h"
+/*
+ *  This sketch sends a message to a TCP server
+ *
+ */
+
+#include <WiFi.h>
 #include <WiFiMulti.h>
 
 WiFiMulti WiFiMulti;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     delay(10);
 
     // We start by connecting to a WiFi network
-    WiFiMulti.addAP("Human-A1-721-2G_EXT", "bsys12bsys34");
+    WiFiMulti.addAP("SSID", "passpasspass");
 
     Serial.println();
     Serial.println();
     Serial.print("Wait for WiFi... ");
 
     while(WiFiMulti.run() != WL_CONNECTED) {
-            Serial.print(".");
-            delay(500);
+        Serial.print(".");
+        delay(500);
     }
 
     Serial.println("");
@@ -29,9 +34,12 @@ void setup() {
 }
 
 
-void loop() {
-    const uint16_t port = 5204;
-    const char * host = "10.31.1.100"; // ip or dns
+void loop()
+{
+    const uint16_t port = 80;
+    const char * host = "192.168.1.1"; // ip or dns
+
+
 
     Serial.print("connecting to ");
     Serial.println(host);
@@ -40,10 +48,10 @@ void loop() {
     WiFiClient client;
 
     if (!client.connect(host, port)) {
-            Serial.println("connection failed");
-            Serial.println("wait 5 sec...");
-            delay(5000);
-            return;
+        Serial.println("connection failed");
+        Serial.println("wait 5 sec...");
+        delay(5000);
+        return;
     }
 
     // This will send the request to the server
@@ -59,3 +67,4 @@ void loop() {
     Serial.println("wait 5 sec...");
     delay(5000);
 }
+
